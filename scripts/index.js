9dbb60e86bab834c8ac5f338af5ebdd2bc5fb7de
ASP <a.sharshovaparshina@gmail.com>
2025-03-19 22:16:07 +0300

    Версия 1 ПР5

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const content = document.querySelector('.content');
+const placelist = content.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+function createCard(name, link, removeCard) {
+    const newcard = cardTemplate.querySelector('.card').cloneNode(true);
+
+    newcard.querySelector('.card__image').src = link;
+    newcard.querySelector('.card__image').alt = name;
+    newcard.querySelector('.card__title').textContent = name;
+
+    newcard.querySelector('.card__delete-button').addEventListener('click', removeCard);
+    
+    placelist.append(newcard); 
+}
+
 // @todo: Функция удаления карточки
 
+function removeCard () { 
+    const card = placelist.querySelector('.card');
+    
+    card.remove();
+}
+
+
 // @todo: Вывести карточки на страницу
+
+ for (let i = 0; i < initialCards.length; i++) {  
+    createCard(initialCards[i].name, initialCards[i].link, removeCard);
+ }
